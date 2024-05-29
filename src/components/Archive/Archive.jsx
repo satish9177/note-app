eec65d414c56498f2087219cdd526df110687201
satish9177 <satishkommanaboina@gmail.com>
2024-05-29 23:54:22 +0530

    third commit

--- src/components/Archive/Archive.jsx
+++ src/components/Archive/Archive.jsx
@@ -6,7 +6,7 @@ const Archive = () => {
     <> 
     <div className='flex flex-wrap mt-28 ml-48 gap-3'>
      { 
-      
+      // eslint-disable-next-line
      data?.length>0?data.filter((item,i) => { if(item.isArchive && !item.del) return item }).map((item,i)=>{
       return <Pin key={item.id} title={item.title} note={item.note} pinned={Pinned} id={item.id} archive={item.isArchive} isArchive={Archive} del={item.del} Del={Delete}/>
    }): <div>
